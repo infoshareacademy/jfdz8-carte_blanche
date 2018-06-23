a0b85c491e8ed807e9092fafc71ea152a5533abe
Kajetan Kasperuk <kajetankasperuk@gmail.com>
2018-06-23 08:05:07 +0200

    working responsive menu nad menu highlights

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-document.querySelectorAll('a[href^="#"]')
-    .forEach(function(anchor) {
-        anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+function smoothScroll() {
+    document.querySelectorAll('a[href^="#"]')
+        .forEach(function (anchor) {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
         });
-    });
-});
+} smoothScroll();
 
 function MouseRollover(MyImage) {
     MyImage.src = "assets/images/ewasiwek.jpg";
@@ -28,3 +30,47 @@ function showMenu() {
         }
     })
 } showMenu();
+
+
+
+function showActiveMenu() {
+    var allAnchors = document.querySelectorAll('.navigation__navigation-list-item>[href^="#"]');
+    var sections = [];
+
+    allAnchors.forEach(function (anchor) {
+        var selector = anchor.getAttribute('href');
+        var section = document.querySelector(selector);
+        sections.push(section);
+    });
+
+    window.addEventListener('scroll', function () {
+        var offset = window.pageYOffset;
+        // console.log(offset);
+        sections.forEach(function (section, index) {
+            var classList = allAnchors[index].classList;
+            if (getElemDistance(section) <= offset+70) {
+                classList.add('active');
+            } else {
+                classList.remove('active');
+            }
+        });
+
+        document.querySelectorAll('.active').forEach(function (element, index, allElements) {
+            if (index === allElements.length - 1) {
+                return;
+            }
+            element.classList.remove('active');
+        })
+    });
+
+    function getElemDistance(elem) {
+        var location = 0;
+        if (elem.offsetParent) {
+            do {
+                location += elem.offsetTop;
+                elem = elem.offsetParent;
+            } while (elem);
+        }
+        return location >= 0 ? location : 0;
+    }
+} showActiveMenu();
