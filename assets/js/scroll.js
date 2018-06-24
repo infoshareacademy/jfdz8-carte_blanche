3c93d7ede251eb6e2749d6a9992b3f40a1d7639c
Ewa Siwek <ew.siwek@gmail.com>
2018-06-24 11:01:51 +0200

    fixed photo animation on scroll

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -8,19 +8,24 @@ var getElemDistance = function (elem) {
     }
     return location >= 0 ? location : 0;
 };
+
 var photo = document.querySelectorAll('.team-item__photo');
+
 function animatedPhoto() {
     photo.forEach(function(photo) {
+
         var photoTop = getElemDistance(photo);
         var visibleTeam = window.scrollY + window.innerHeight - photo.height / 2;
         var photoBottom = (photoTop + photo.height);
         var visibleHalfTeam = visibleTeam > photoTop;
         var isNotScrolledPast = window.scrollY < photoBottom;
+
         if (visibleHalfTeam && isNotScrolledPast) {
-            photo.classList.add('active');
+            photo.classList.add('photo__active');
         } else {
-            photo.classList.remove('active');
+            photo.classList.remove('photo__active');
         }
     })
 }
+
 window.addEventListener('scroll', animatedPhoto);
